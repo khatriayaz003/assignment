6b6b65ce875981e414a8d969e7d0aee8d9c939ad
khatriayaz003 <khatri.ayaz003@gmail.com>
2019-05-16 00:01:07 +0800

    Initial Commit

---
+++ assignment/assignment/src/main/java/com/assignment/speedrent/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.assignment.speedrent.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.assignment.speedrent.vo.Car;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public Car hello() {
+		Car car = new Car("Abc", "Xyz");
+		return car;
+	}
+
+}
